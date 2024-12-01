870fda5c0bb142e1b1247d2747896ddb766300e4
daisukedayo <faculty.miyakoda@gmail.com>
2024-12-01 14:18:40 +0900

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import LikeButton from "./like-button";
+
+// const DevHead = ({ title }: { title: string }): string => title;
+
+export default function Home() {
+  return <div>aaaaaaa</div>;
+}
